14f60ce57876f9529b7657ca50c9cc241083df3a
Akmal Doghar <84Pakarmy@gmail.com>
2025-08-10 13:37:16 +0500

    initial commit

--- ai-chatbot-home/next.config.mjs
+++ ai-chatbot-home/next.config.mjs
@@ -1,8 +1,6 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 👈 this is the key part!
+  // output: 'export', // is line ko hatao ya comment karo
 };
 
 export default nextConfig;
